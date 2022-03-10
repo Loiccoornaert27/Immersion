3989a4f87d78b66fc96d9a9cb22632c260b55b30
kevinverdonck <94345886+kevinverdonck@users.noreply.github.com>
2022-03-10 13:40:27 +0100

    Create Class SoftSkill

---
+++ Back/ImmersionApi/ImmersionApi/Models/SoftSkill.cs
@@ -0,0 +1,18 @@
+﻿namespace ImmersionApi.Models
+{
+    public class SoftSkill
+    {
+        private int _id;
+        private static int _count;
+        private List<SoftSkill>? _listSoftSkill;
+
+        public int Id { get => _id; set => _id = value; }
+        public static int Count { get => _count; set => _count = value; }
+        public List<SoftSkill>? ListSoftSkill { get => _listSoftSkill; set => _listSoftSkill = value; }
+
+        public SoftSkill()
+        {
+            _id = ++_count;
+        }
+    }
+}
