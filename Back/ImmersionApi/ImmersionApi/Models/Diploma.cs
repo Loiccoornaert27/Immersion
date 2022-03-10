479fc56ff3689e010166122e10988db0bdad60a5
Kamoullox <62745308+Kamoullox@users.noreply.github.com>
2022-03-10 14:20:18 +0100

    :sparkles: Ajout du Model / Controller / DiplomaFakeDB pour les diplome

---
+++ Back/ImmersionApi/ImmersionApi/Models/Diploma.cs
@@ -0,0 +1,15 @@
+﻿namespace ImmersionApi.Models
+{
+    public class Diploma
+    {
+        private static int Count;
+
+        public int? ID { get; set; }
+        public string Name { get; set; }
+
+        public Diploma()
+        {
+            ID = ++Count;
+        }
+    }
+}
