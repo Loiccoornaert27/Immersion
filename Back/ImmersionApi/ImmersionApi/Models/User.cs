834ce6dc87387d14ed39be96fdeaf169beadb99e
Kamoullox <62745308+Kamoullox@users.noreply.github.com>
2022-03-10 16:10:32 +0100

    :sparkles: User: Ajout du controller, model et fakeDB de user

---
+++ Back/ImmersionApi/ImmersionApi/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ImmersionApi.Models
+{
+    public class User
+    {
+        public int? ID { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Password { set ; get; }
+        public bool? IsAdmin { get; set; }
+        public string? Avatar { get; set; }
+
+        private static int Count { get; set; }
+
+        public User()
+        {
+            ID = ++Count;
+        }
+    }
+}
