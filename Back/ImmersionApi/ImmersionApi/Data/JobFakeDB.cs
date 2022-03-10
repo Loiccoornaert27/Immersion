4d324e2361c15443ede2fb4e6d963a6dcceca780
Kamoullox <62745308+Kamoullox@users.noreply.github.com>
2022-03-10 17:15:56 +0100

    :sparkles: Job: Ajout du controller, model et Fake DB pour les jobs

---
+++ Back/ImmersionApi/ImmersionApi/Data/JobFakeDB.cs
@@ -0,0 +1,54 @@
+﻿using ImmersionApi.Models;
+
+namespace ImmersionApi.Data
+{
+    public class JobFakeDB
+    {
+        private List<Job> _jobs;
+
+        public JobFakeDB()
+        {
+            _jobs = new List<Job>();
+        }
+
+        public List<Job> GetAll()
+        {
+            return _jobs;
+        }
+
+        public Job GetById(int id)
+        {
+            return _jobs.FirstOrDefault((x) => x.ID == id);
+        }
+
+        public Job Add(Job job)
+        {
+            _jobs.Add(job);
+
+            return job;
+        }
+
+        public Job Edit(Job newJob, int id)
+        {
+            Job toEdit = GetById(id);
+
+            if (toEdit == null) return null;
+
+            toEdit.Description = newJob.Description ?? toEdit.Description;
+            toEdit.Category = newJob.Category;
+            toEdit.BeginDate = newJob.BeginDate;
+            toEdit.Name = newJob.Name ?? toEdit.Name;
+
+            return toEdit;
+        }
+
+        public bool RemoveById(int id)
+        {
+            var job = GetById(id);
+
+            if (job == null) return false;
+
+            return _jobs.Remove(job);
+        }
+    }
+}
