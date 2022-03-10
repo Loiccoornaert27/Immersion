28fab7178260890d1baf38796a47cd654e5540da
kevinverdonck <94345886+kevinverdonck@users.noreply.github.com>
2022-03-10 13:54:59 +0100

    Create SoftSkillFakeDb.cs

---
+++ Back/ImmersionApi/ImmersionApi/Data/SoftSkillFakeDb.cs
@@ -0,0 +1,50 @@
+﻿using ImmersionApi.Models;
+
+namespace ImmersionApi.Data
+{
+    public class SoftSkillFakeDb
+    {
+        private List<SoftSkill> _listSoftSkill { get; set; }
+
+        public SoftSkillFakeDb()
+        {
+            _listSoftSkill = new();
+        }
+
+        public List<SoftSkill> GetAllSoftSkill()
+        {
+            return _listSoftSkill;
+        }
+
+        public SoftSkill GetSoftSkillById(int id)
+        {
+            return _listSoftSkill.FirstOrDefault(x => x.Id == id);
+        }
+
+        public SoftSkill AddSoftSkill(SoftSkill newSoftSkill)
+        {
+            _listSoftSkill.Add(newSoftSkill);
+            return newSoftSkill;
+        }
+
+        public bool RemoveSoftSkill(int id)
+        {
+            SoftSkill found = GetSoftSkillById(id);
+
+            if (found == null) return false;
+            return _listSoftSkill.Remove(found);         
+        }
+
+        public SoftSkill EditSoftSkill(int id, SoftSkill newSoftSkill)
+        {
+            SoftSkill found = GetSoftSkillById(id);
+
+            if (found == null) return null;
+            else
+            {
+                found.ListSoftSkill = newSoftSkill.ListSoftSkill;
+                return found;
+            }
+        }
+    }
+}
