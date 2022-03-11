aa501214d5de6ee6eb8d482bc478ac4dfdb10361
Kamoullox <62745308+Kamoullox@users.noreply.github.com>
2022-03-11 13:46:34 +0100

    Ajout de la nouvelle logique d'addition des profils utilisateur

--- Back/ImmersionApi/ImmersionApi/Models/UserProfil.cs
+++ Back/ImmersionApi/ImmersionApi/Models/UserProfil.cs
@@ -4,5 +4,8 @@
     {
         public int ID { get; set; }
         public Job Job { get; set; }
+        public List<int> SoftSkills { get; set; }
+        public List<int> HardSkills { get; set; }
+        public List<int> Diplomas { get; set; }
     }
 }
