a925a33387154c133633bb53381eaf953c8a57b4
Loic Coornaert <loic.coornaert27@gmail.com>
2022-03-11 14:31:56 +0100

    Ajout modèle et db mock pour le profil type

---
+++ Back/ImmersionApi/ImmersionApi/Controllers/RegularProfilController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImmersionApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegularProfilController : ControllerBase
+    {
+    }
+}
---
+++ Back/ImmersionApi/ImmersionApi/Data/RegularProfilFakeDB.cs
@@ -0,0 +1,54 @@
+﻿using ImmersionApi.Models;
+
+namespace ImmersionApi.Data
+{
+    public class RegularProfilFakeDB
+    {
+        private List<RegularProfil> _regularProfils;
+
+        public RegularProfilFakeDB()
+        {
+            _regularProfils = new List<RegularProfil>();
+        }
+
+        public List<RegularProfil> GetAll()
+        {
+            return _regularProfils;
+        }
+
+        public RegularProfil GetById(int id)
+        {
+            return _regularProfils.FirstOrDefault((x) => x.ID == id);
+        }
+
+        public RegularProfil Add(RegularProfil regularProfil)
+        {
+            _regularProfils.Add(regularProfil);
+
+            return regularProfil;
+        }
+
+        public RegularProfil Edit(RegularProfil newRegularProfil, int id)
+        {
+            RegularProfil toEdit = GetById(id);
+
+            if (toEdit == null) return null;
+
+            toEdit.Job = newRegularProfil.Job;
+            toEdit.SoftSkills = newRegularProfil.SoftSkills;
+            toEdit.HardSkills = newRegularProfil.HardSkills;
+            toEdit.Diplomas = newRegularProfil.Diplomas;
+
+            return toEdit;
+        }
+
+        public bool RemoveById(int id)
+        {
+            var regularProfil = GetById(id);
+
+            if (regularProfil == null) return false;
+
+            return _regularProfils.Remove(regularProfil);
+        }
+    }
+}
